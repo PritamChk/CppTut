1ec94ea957cb1affe296835ac71a419ad3ba2753
Pritam Chakraborty <pritamchakraborty2017@gmail.com>
2020-10-23 10:58:25 +0530

    comments added

--- DataTypesInCpp.cpp
+++ DataTypesInCpp.cpp
@@ -17,7 +17,8 @@ int main(){
     int valInt = 200;       //declaration and definition
     float Fraction = 6.03;  //declaration and definition
     double lombaFraction = 9.00007; //declaration and definition
-
+    
+    //Print Statements for each data type
     cout<<"Bool VAl: "<<booleanVal<<" => TRUE and sizeof bool = "<<sizeof(bool)<<" Byte(s)"<<endl; // here () are necessary, when using datatype name as parameter to it.
     
     cout<<"Char VAl: "<<singleChar<<" and sizeof Char = "<<sizeof singleChar<<" Byte(s)"<<endl; // see that we can write sizeof in two ways
@@ -27,5 +28,6 @@ int main(){
     cout<<"Float VAl: "<<Fraction<<" and sizeof Float = "<<sizeof(int)<<" Byte(s)"<<endl; 
 
     cout<<"Double VAl: "<<lombaFraction<<" and sizeof Double = "<<sizeof(int)<<" Byte(s)"<<endl; 
+    //end of print statements
     return 0;
 }
