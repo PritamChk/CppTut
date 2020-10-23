9ea37813d803d29dfae8f6bdb1a4009831bc305b
Pritam Chakraborty <pritamchakraborty2017@gmail.com>
2020-10-23 11:16:22 +0530

    modifier wrong example

--- DataTypesInCpp.cpp
+++ DataTypesInCpp.cpp
@@ -31,8 +31,14 @@ int main(){
     //end of print statements
     //Declaration can be done in any place in C++
     //Now comes example of modifiers
-    short var_1 = 256;
-    cout<<endl<<"var 1 =  "<<var_1<<sizeof var_1<<endl; 
-    
+    short var_1 = 65600; /*
+        $ g++ DataTypesInCpp.cpp 
+        DataTypesInCpp.cpp: In function ‘int main()’:
+        DataTypesInCpp.cpp:34:19: warning: overflow in conversion from ‘int’ to ‘short int’ changes value from ‘65600’ to ‘64’ [-Woverflow]
+        34 |     short var_1 = 65600;
+           |   
+    */
+    cout<<endl<<"var 1 =  "<<var_1<<" Size of var 1: "<<sizeof var_1<<" Bytes"<<endl; 
+     //Output : var 1 =  64 Size of var 1: 2 Bytes
     return 0;
 }
