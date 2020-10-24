151d6296988b5a83e644285ffafe1bebb80da761
Pritam Chakraborty <pritamchakraborty2017@gmail.com>
2020-10-24 20:50:23 +0530

    File one.cpp and two.cpp have been created to experiment with extern storage classifier

---
+++ one.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//#
+//using namespace std;
+
+char ch;
+int x,y;
+
+void func1();
+
+int main(){
+    std::cout<<"Hello world from main";
+    return 0;
+}
+
+void func(){
+    x = 123;
+}
---
+++ two.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#define newline std::endl
+//#
+//using namespace std;
+
+extern char ch;
+extern int x,y;
+
+void func22();
+void func23();
+void func22(){
+    std::cout<<"Hello world from main"<<newline;
+}
+
+void func23(){
+    std::cout<<"From Function 23(File two.cpp)"<<newline;
+    y = 69;
+}
