3d3d2d47aa63ccc32d51ed1b31e8cebdaccd8b30
Pritam Chakraborty <pritamchakraborty2017@gmail.com>
2020-10-24 22:13:25 +0530

    ifndef & ifdef understood

--- preprocessors.cpp
+++ preprocessors.cpp
@@ -6,3 +6,28 @@ CPP supports following preprocessors
 #inclide    #line       #pragma     #undef
 --------------------------------------------------
 */
+#include<iostream>
+
+//#define DEBUG //to DEBUG FLAG use [g++ -D DEBUG preprocessors.cpp] from cmd
+//else to on DEBUG uncomment previous line
+
+#ifndef print
+#define print std::cout
+#endif
+
+#ifndef scan
+#define scan std::cin
+#endif
+
+#ifndef newline
+#define newline std::endl
+#endif
+
+
+int main(void){
+    print<<"Hello ifndef"<<newline;
+    #ifdef DEBUG 
+        print<<"as DEBUG FLAG is on, so ifdef is working"<<newline;
+    #endif    
+    return 0;
+}
