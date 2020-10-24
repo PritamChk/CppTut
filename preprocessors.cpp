cd574d7fe51bcfa14e3c1f37fe79e2fa6224e73c
Pritam Chakraborty <pritamchakraborty2017@gmail.com>
2020-10-24 21:18:21 +0530

    new file preprocessor created

---
+++ preprocessors.cpp
@@ -0,0 +1,8 @@
+/*
+CPP supports following preprocessors
+-------------------------------------------------
+#define     #elif       #else       #endif
+#error      #if         #ifdef      #ifndef
+#inclide    #line       #pragma     #undef
+--------------------------------------------------
+*/
